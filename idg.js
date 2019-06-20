dfddf5e5821604d01572f73cb4d904983dde0569
westerschmal <30859973+westerschmal@users.noreply.github.com>
2019-06-20 23:58:07 +0200

    Add files via upload

---
+++ idg.js
@@ -0,0 +1,205 @@
+var IDGdfpAds = {};
+
+googletag.cmd.push(function() {
+
+    var pubads = googletag.pubads();
+
+IDGdfpAds['ad_layer'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[1, 1], [640, 480]], 'ad_layer').setTargeting('NL_Position','ad_layer').addService(googletag.pubads());
+            if (Modernizr.mq('only screen and (min-width:1700px)')) {
+IDGdfpAds['ad_billboard'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[2,2], [728, 90], [970, 250]], 'ad_billboard').setTargeting('NL_Position','ad_billboard').addService(googletag.pubads());
+IDGdfpAds['ad_skyscraper'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[120, 600], [160, 600]], 'ad_skyscraper').setTargeting('NL_Position','ad_skyscraper').addService(googletag.pubads());
+IDGdfpAds['ad_adv1'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[555, 112],[555,125]], 'ad_adv1').setTargeting('NL_Position','ad_adv1').addService(googletag.pubads());
+IDGdfpAds['ad_in_page_video'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[545,306],[2,1]], 'ad_in_page_video').setTargeting('NL_Position','ad_in_page_video').addService(googletag.pubads());
+IDGdfpAds['ad_mpu'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[120, 600], [160, 600], [300,250], [300, 600]], 'ad_mpu').setTargeting('NL_Position','ad_mpu').addService(googletag.pubads());
+IDGdfpAds['ad_interscroller'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[970, 252]], 'ad_interscroller').setTargeting('NL_Position','ad_interscroller').addService(googletag.pubads());
+IDGdfpAds['ad_leaderboard-bottom'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[728, 90]], 'ad_leaderboard-bottom').setTargeting('NL_Position','ad_leaderboard-bottom').addService(googletag.pubads());
+IDGdfpAds['ad_skin-left'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1200]], 'ad_skin-left').setTargeting('NL_Position','ad_skin-left').addService(googletag.pubads());
+IDGdfpAds['ad_skin-right'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1201]], 'ad_skin-right').setTargeting('NL_Position','ad_skin-right').addService(googletag.pubads());
+IDGdfpAds['ad_leaderboard-top'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[728, 90],[1800, 1000]], 'ad_leaderboard-top').setTargeting('NL_Position','ad_leaderboard-top').addService(googletag.pubads());
+            } else if (Modernizr.mq('only screen and (min-width:1241px)')) {
+IDGdfpAds['ad_billboard'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[2,2], [728, 90], [970, 250]], 'ad_billboard').setTargeting('NL_Position','ad_billboard').addService(googletag.pubads());
+IDGdfpAds['ad_adv1'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[555, 112],[555,125]], 'ad_adv1').setTargeting('NL_Position','ad_adv1').addService(googletag.pubads());
+IDGdfpAds['ad_in_page_video'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[545,306],[2,1]], 'ad_in_page_video').setTargeting('NL_Position','ad_in_page_video').addService(googletag.pubads());
+IDGdfpAds['ad_mpu'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[120, 600], [160, 600], [300,250], [300, 600]], 'ad_mpu').setTargeting('NL_Position','ad_mpu').addService(googletag.pubads());
+IDGdfpAds['ad_interscroller'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[970, 252]], 'ad_interscroller').setTargeting('NL_Position','ad_interscroller').addService(googletag.pubads());
+IDGdfpAds['ad_leaderboard-bottom'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[728, 90]], 'ad_leaderboard-bottom').setTargeting('NL_Position','ad_leaderboard-bottom').addService(googletag.pubads());
+IDGdfpAds['ad_skin-left'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1200]], 'ad_skin-left').setTargeting('NL_Position','ad_skin-left').addService(googletag.pubads());
+IDGdfpAds['ad_skin-right'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1201]], 'ad_skin-right').setTargeting('NL_Position','ad_skin-right').addService(googletag.pubads());
+IDGdfpAds['ad_leaderboard-top'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[728, 90],[1800, 1000]], 'ad_leaderboard-top').setTargeting('NL_Position','ad_leaderboard-top').addService(googletag.pubads());
+            } else if (Modernizr.mq('only screen and (min-width:1000px)')) {
+IDGdfpAds['ad_billboard'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[2,2], [728, 90], [970, 250]], 'ad_billboard').setTargeting('NL_Position','ad_billboard').addService(googletag.pubads());
+IDGdfpAds['ad_adv1'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[555, 112],[555,125]], 'ad_adv1').setTargeting('NL_Position','ad_adv1').addService(googletag.pubads());
+IDGdfpAds['ad_in_page_video'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[545,306],[2,1]], 'ad_in_page_video').setTargeting('NL_Position','ad_in_page_video').addService(googletag.pubads());
+IDGdfpAds['ad_mpu'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[120, 600], [160, 600], [300,250], [300, 600]], 'ad_mpu').setTargeting('NL_Position','ad_mpu').addService(googletag.pubads());
+IDGdfpAds['ad_interscroller'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[970, 252]], 'ad_interscroller').setTargeting('NL_Position','ad_interscroller').addService(googletag.pubads());
+IDGdfpAds['ad_leaderboard-bottom'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[728, 90]], 'ad_leaderboard-bottom').setTargeting('NL_Position','ad_leaderboard-bottom').addService(googletag.pubads());
+IDGdfpAds['ad_skin-left'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1200]], 'ad_skin-left').setTargeting('NL_Position','ad_skin-left').addService(googletag.pubads());
+IDGdfpAds['ad_skin-right'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[160, 1201]], 'ad_skin-right').setTargeting('NL_Position','ad_skin-right').addService(googletag.pubads());
+            } else if (Modernizr.mq('only screen and (min-width:780px)')) {
+IDGdfpAds['ad_mpu-mob'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[300, 250], [300, 600]], 'ad_mpu-mob').setTargeting('NL_Position','ad_mpu-mob').addService(googletag.pubads());
+            } else if (Modernizr.mq('only screen and (min-width:500px)')) {
+IDGdfpAds['ad_mpu-mob'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[300, 250], [300, 600]], 'ad_mpu-mob').setTargeting('NL_Position','ad_mpu-mob').addService(googletag.pubads());
+            } else if (Modernizr.mq('only screen and (max-width:499px)')) {
+IDGdfpAds['ad_mpu-mob'] = googletag.defineSlot('/8456/IDG.NL_B2B_WebWereld.nl/homepage_door',[[300, 250], [300, 600]], 'ad_mpu-mob').setTargeting('NL_Position','ad_mpu-mob').addService(googletag.pubads());
+            }
+
+})
+
+
+        function loadPersonalizedDfp() {
+            fob.dfp = {}
+
+            $.ajax('/ajax/dfp-user-data?hash=' + getParameterByName('hash'), {
+                cache : false,
+                dataType : 'json',
+                success: function(data, text, xhr) {
+                    fob.dfp = {
+                        data : data.data,
+                        hash : data.hash,
+                    };
+                },
+                complete : function() {
+                    googletag.cmd.push(function() {
+                        
+        var pubads = googletag.pubads();
+
+        
+        pubads.collapseEmptyDivs(true);
+
+        pubads.addEventListener('slotRenderEnded', function(e) {
+            if ($('#ad_mpu div').height() >= 600) {
+                $('#one #right').addClass('large-ad');
+            }
+            
+            if ($('.article-list-simple .advertorial').height() != 0) {
+                $('.article-list-simple .advertorial').addClass('active');
+            }
+        });
+
+    
+                        if (fob.dfp.data && typeof fob.dfp.data === 'object') {
+                            Object.keys(fob.dfp.data).forEach(function(k) {
+                                googletag.pubads().setTargeting(k, fob.dfp.data[k]);
+                            });
+                        }
+
+                        console.log('Googletag services enabled');
+                        // defineAdUnits is a function defined by Adhese when this script has been loaded
+                        typeof defineAdUnits === 'function' ? defineAdUnits() : googletag.pubads().refresh();
+                            
+                    });
+                },
+                error : function(...args) {
+                    console.log(args);
+                    console.log('An error occurred while trying to set DFP user data');
+                }
+            });
+        }
+
+        // Consent Management
+        window.admiral = window.admiral || function() {(admiral.q = admiral.q || []).push(arguments)};
+        
+        var debug = false;
+        window.idgnl = window.idgnl || {};
+        window.idgnl.cmp = (function() { 
+            var debug = false;
+            var PERSONALIZATION_PURPOSE_ID = 2; 
+            
+            var checkIfInEU = new Promise(function(resolve, reject) {
+                var promiseResolved = false;
+                setTimeout(function() {
+                    if(debug) { console.log('GDPR checkIfInEU timeout reached promiseResolved', promiseResolved); }
+                    if (!promiseResolved) {
+                        promiseResolved=true;
+                        resolve(); // effectively in eu (most conservative)
+                    }
+                }, 10000);
+                
+                window.admiral('after', 'cmp.loaded', function(e) {
+                    if (promiseResolved) { return; }
+                    promiseResolved = true;
+                    
+                    if (debug) { console.log('GDPR checkIfInEU - cmp.loaded', e); }
+                    var isEuVisitor = e.euVisitor || false;
+                    if (isEuVisitor) { resolve(); }
+                    else { reject(); }
+                });
+            });
+            
+            // util to check for 1 purpose or 1 vendor in consent data structure
+            var checkConsentData = function(ConsentData, purposeId, vendorId){
+                var _ConsentData = {standardPurposes:{}}; // normalise due to different naming conventions        
+                if ('standardPurposes' in ConsentData) { // prefer standardPurposes as defined on PublisherConsents which is not global and does not return early
+                    _ConsentData.standardPurposes = ConsentData.standardPurposes
+                } else if('purposeConsents' in ConsentData) { //comes from Vendor Consents
+                    _ConsentData.standardPurposes = ConsentData.purposeConsents
+                }
+                _ConsentData.vendorConsents = ConsentData.vendorConsents || {}; //comes from VendorConsents
+                _ConsentData.customPurposes = ConsentData.customPurposes || {}; //comes from PublisherConsents
+                if (debug) { console.log('GDPR', 'checkConsentData', 'purposeId', purposeId, 'vendorId', vendorId, _ConsentData); }
+
+                if (typeof vendorId !== 'undefined'){
+                    if (vendorId in _ConsentData.vendorConsents && _ConsentData.vendorConsents[''+vendorId]){            
+                        return true;
+                    }        
+                    //vendor id match is wanted but is false or not defined
+                    return false;
+                }
+                
+                if (typeof purposeId !== 'undefined' && purposeId !== null){
+                    
+                    if( (purposeId in _ConsentData.standardPurposes && _ConsentData.standardPurposes[''+purposeId]) || 
+                        (purposeId in _ConsentData.customPurposes && _ConsentData.customPurposes[''+purposeId])
+                    ) {
+                        return true;
+                    }
+                }
+        
+                if (debug) { console.log('GDPR','checkConsentData',false); }
+                return false;
+            };
+            
+            //util create promise for cmp for a purposeid
+            var checkCMPForPurposeConsent = function(purposeId) {
+                return new Promise(function (resolve, reject) {
+                        checkIfInEU.then(function() {
+                            __cmp('getPublisherConsents', [], function(PublisherConsents, success){
+                                if (debug) {
+                                    console.log('GDPR',' checkCMPForPurposeConsent - PublisherConsents ','purposeId', purposeId, 'PublisherConsents=' ,PublisherConsents, 'success=', success);
+                                }
+                                resolve(checkConsentData(PublisherConsents,purposeId));
+                            });
+                        }).catch(function(){
+                            resolve(true);
+                        });
+                    });
+            };
+                
+            return {
+                 isDebug : function() { return debug; },
+                 adsense : checkCMPForPurposeConsent(PERSONALIZATION_PURPOSE_ID),
+                 gpt : checkCMPForPurposeConsent(PERSONALIZATION_PURPOSE_ID)
+            };
+        })();
+
+        googletag.cmd = googletag.cmd || [];
+        
+        if (window.idgnl.cmp) {
+            if (debug) { console.log('GDPR','gpt.js delayed loading'); }
+                       
+            window.idgnl.cmp.gpt.then( function(hasConsent) {
+                googletag.enableServices(); 
+
+                if (hasConsent) {
+                    loadPersonalizedDfp();
+                } else { 
+                    googletag.cmd.unshift(function() {
+                        if (debug) { console.log('GDPR','setRequestNonPersonalizedAds set'); }
+                        googletag.pubads().setRequestNonPersonalizedAds(1);
+                        googletag.pubads().refresh();
+                    });
+                }
+            });
+        } else {
+            if (debug) { console.log('GDPR','gpt.js delayed loading skipped'); }
+        }
